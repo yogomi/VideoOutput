305dddc1726fd86d5d95098438bcfdaaf1f531fe
Makoto Yano <yan133@gmail.com>
2014-02-03 02:28:23 +0900

    The function which outputs an video image is added.

--- video_output.cc
+++ video_output.cc
@@ -6,10 +6,28 @@ int main(int argc, char** argv) {
   cv::Mat frame;
   cv::namedWindow("video", 1);
   cv::VideoCapture cap(0);
+
   if (!cap.isOpened()) {
     std::cout << "Cannot open camera" << std::endl;
     return -1;
   }
+
+  cv::VideoWriter output;
+  int codec =  CV_FOURCC('m', 'p', '4', 'v');
+  cv::Size capture_size =
+    cv::Size(static_cast<int>(cap.get(CV_CAP_PROP_FRAME_WIDTH)),
+          static_cast<int>(cap.get(CV_CAP_PROP_FRAME_HEIGHT)));
+
+  std::cout << "Size width = " << capture_size.width << std::endl;
+  std::cout << "Size height = " << capture_size.height << std::endl;
+  output.open("output.m4v", codec, 30
+              , capture_size);
+
+  if (!output.isOpened()) {
+    std::cout << "Cannot open video output" << std::endl;
+    return -1;
+  }
+
   while (true) {
     cap >> frame;
     if (frame.empty()) {
@@ -19,6 +37,9 @@ int main(int argc, char** argv) {
     if (cv::waitKey(30) >= 0) {
       break;
     }
+    output << frame;
   }
+  output.release();
+  cap.release();
   return 0;
 }
