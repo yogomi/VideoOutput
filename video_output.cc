59b9eda1e63c32ba76706e4bb650138fcfefe324
Makoto Yano <yan133@gmail.com>
2014-02-02 17:18:55 +0900

    Apply google coding style.

--- video_output.cc
+++ video_output.cc
@@ -1,23 +1,22 @@
+// Copyright 2014 Makoto Yano
+
 #include <opencv2/opencv.hpp>
 
-using namespace cv;
-
-int main(int argc, char** argv)
-{
-  Mat frame;
-  namedWindow("video", 1);
-  VideoCapture cap(0);
+int main(int argc, char** argv) {
+  cv::Mat frame;
+  cv::namedWindow("video", 1);
+  cv::VideoCapture cap(0);
   if (!cap.isOpened()) {
     std::cout << "Cannot open camera" << std::endl;
     return -1;
   }
   while (true) {
     cap >> frame;
-    if(frame.empty()) {
+    if (frame.empty()) {
       break;
     }
-    imshow("video", frame);
-    if (waitKey(30) >= 0) {
+    cv::imshow("video", frame);
+    if (cv::waitKey(30) >= 0) {
       break;
     }
   }
